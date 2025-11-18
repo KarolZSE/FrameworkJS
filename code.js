a7127bef7c726e389cfddbd431a7360f359117a5
KarolZSE <karol.sulek@zse.krakow.pl>
2025-11-18 14:27:18 +0100

    added a mining mechanic, fixed player going out of bounds, centered the game

--- code.js
+++ code.js
@@ -6,46 +6,57 @@ for (let i = 0; i < 15; i++) {
         const block = document.createElement('div');
 
         if (Stones[i][Math.max(0, j - 1)] && Math.random() > 0.2) {
-            block.classList.add('wall');
-            Stones[i][j] = 1;
+            WallMaking();
+        } else if (Math.random() > 0.8) {
+            WallMaking();
         };
 
-        if (Math.random() > 0.8) {
+        function WallMaking() {
             block.classList.add('wall');
             Stones[i][j] = 1;
+
+            let destruction_level = 0;
+            block.addEventListener('click', () => {
+                if (++destruction_level >= 4) {
+                    Stones[i][j] = 0;
+                    block.classList.remove('wall');
+                    block.textContent = '';
+                } else {
+                    block.textContent = destruction_level;
+                }   
+            });
         };
 
         GameDir.appendChild(block);
     }
 }
 
-let x = 0, y = 15;
+let x = 0, y = 0;
 const Player = document.getElementById('Player');
 document.addEventListener('keydown', (e) => {
     if (e.key.toLowerCase() == 'w') {
-        if (Stones[y - 1][x]) return;
+        if (y - 1 < 0 || Stones[y - 1][x]) return;
         y--;
         Player.style.top = Number(Player.offsetTop) - 40 + 'px';
     };
 
     if (e.key.toLowerCase() == 's') {
-        if (Stones[y + 1][x]) return;
+        if (y + 1 >= 15 || Stones[y + 1][x]) return;
         y++;
         Player.style.top = Number(Player.offsetTop) + 40 + 'px';
     };
 
     if (e.key.toLowerCase() == 'a') {
-        if (Stones[y][x - 1]) return;
+        if (x - 1 < 0 || Stones[y][x - 1]) return;
         x--;
         Player.style.left = Number(Player.offsetLeft) - 40 + 'px';
     };
 
     if (e.key.toLowerCase() == 'd') {
-        if (Stones[y][x + 1]) return;
+        if (x + 1 >= 15 || Stones[y][x + 1]) return;
         x++;
         Player.style.left = Number(Player.offsetLeft) + 40 + 'px';
     };
-
 });
 
 // window.onerror = () => true;
